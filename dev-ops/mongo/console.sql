771ced2352fdf1e0a9370cccab3db2ce5befb3fa
hatrongvu13 <hatrongvu13@gmail.com>
2021-09-21 09:02:15 +0700

    create user roles mongodb

--- dev-ops/mongo/console.sql
+++ dev-ops/mongo/console.sql
@@ -8,3 +8,14 @@ db.createUser(
         ] 
     }
 )
+-- create user and role 
+db.createUser(
+{
+    user: "",
+    pwd: "",
+    roles: [] --
+    -- ["root"]
+    -- [{role: "adminAnyDatabase", db: "admin" }]
+    -- [{role: "dbOwner", db: ""}]
+}
+)
