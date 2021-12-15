799ebec59097e441a13b884606b043af914e8528
hatrongvu13 <hatrongvu13@gmail.com>
2021-12-15 10:54:45 +0700

    update create user mongodb

--- dev-ops/mongo/console.sql
+++ dev-ops/mongo/console.sql
@@ -1,21 +1,20 @@
-use app;
 db.createUser(
-    { 
-        user: "admin", 
-        pwd: "adminpassword", 
-        roles: [
-            { role: "userAdminAnyDatabase", db: "admin" }
-        ] 
-    }
-)
--- create user and role 
-db.createUser(
-{
-    user: "",
-    pwd: "",
-    roles: [] --
-    -- ["root"]
-    -- [{role: "adminAnyDatabase", db: "admin" }]
-    -- [{role: "dbOwner", db: ""}]
-}
+   {
+     user: "",
+     pwd: "",
+     roles: [ {role:"read", db:""} ]
+      //     All build-in Roles 
+      //     Database User Roles: read|readWrite
+      //     Database Admin Roles: dbAdmin|dbOwner|userAdmin
+      //     Cluster Admin Roles: clusterAdmin|clusterManager|clusterMonitor|hostManager
+      //     Backup and Restoration Roles: backup|restore
+      //     All-Database Roles: readAnyDatabase|readWriteAnyDatabase|userAdminAnyDatabase|dbAdminAnyDatabase
+      //     Superuser Roles: root
+      // authenticationRestrictions: [ {
+      //       clientSource: ["192.0.2.0"],
+      //       serverAddress: ["198.51.100.0"]
+      // } ],
+      //mechanisms: [ "SCRAM-SHA-1","SCRAM-SHA-256"], 
+      //passwordDigestor: "server|client"
+   }
 )
