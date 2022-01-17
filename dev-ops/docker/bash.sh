4b7bdd681457c8c933d1550245dda9f9231dca74
hatrongvu13 <hatrongvu13@gmail.com>
2022-01-17 14:53:58 +0700

    add docker build command

--- dev-ops/docker/bash.sh
+++ dev-ops/docker/bash.sh
@@ -2,3 +2,8 @@
 #exec command container
 docker exec -it <container id or container name> /bin/bash
 ##################
+
+## build image custom
+docker build [options] PATH | URL | -
+### build with URL
+ docker build github.com/creack/docker-firefox
